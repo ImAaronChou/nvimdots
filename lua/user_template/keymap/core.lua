169feab34ea683de9501386bbd7e91f949051c07
aaron.zhou <aaron.zhou@narwal.com>
2023-09-06 23:02:29 +0800

    优化nvimtree的按键

--- lua/user_template/keymap/core.lua
+++ lua/user_template/keymap/core.lua
@@ -3,7 +3,4 @@ local map_cr = bind.map_cr
 
 return {
 	["n|<leader>lf"] = map_cr("luafile %"):with_noremap():with_silent():with_desc("config update: luafile %"),
-	["n|<S-h>"] = map_cr("bp"):with_noremap():with_silent():with_desc("tab: Create a new tab"),
-	["n|<S-l>"] = map_cr("bn"):with_noremap():with_silent():with_desc("tab: Create a new tab"),
-	["n|<S-q>"] = map_cr("BufDel"):with_noremap():with_silent():with_desc("tab: Create a new tab"),
 }
