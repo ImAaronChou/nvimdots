29503c20961e8c5b9aaa28e5867ffd26b62c28b1
aaron.zhou <aaron.zhou@narwal.com>
2023-09-06 23:08:17 +0800

    优化nvimtree按键

--- lua/user/configs/nvim-tree.lua
+++ lua/user/configs/nvim-tree.lua
@@ -1,30 +1,16 @@
--- local function on_attach(bufnr)
---     local function move_file_to()
---         local node = api.tree.get_node_under_cursor()
---         local file_src = node['absolute_path']
---         local file_out = vim.fn.input("MOVE TO: ", file_src, "file")
---         local dir = vim.fn.fnamemodify(file_out, ":h")
---         vim.fn.system { 'mkdir', '-p', dir }
---         vim.fn.system { 'mv',  file_src, file_out }
---     end
---     vim.keymap.set('n', 'X', move_file_to, opts('Move File To'))
--- end
-local function GetWindows()
-	return vim.tbl_filter(function(id)
+--用于指定在第几个窗口打开指定文件
+local function open_file_in_specify_window(filename, num)
+	-- 获取当前窗口数量
+	local windows = vim.tbl_filter(function(id)
 		local config = vim.api.nvim_win_get_config(id)
 		local bufnr = vim.api.nvim_win_get_buf(id)
 		return config and config.relative == "" or require("nvim-tree.utils").is_nvim_tree_buf(bufnr)
 	end, vim.api.nvim_list_wins())
-end
-
-local function open_file_in_specify_window(filename, num)
-	-- 获取当前窗口数量
-	local windows = GetWindows()
-	print("windows", vim.inspect(windows))
 
 	local target_window_id = 0
 	-- 如果只有一个窗口，则创建一个新的窗口并打开文件
 	if #windows <= num then
+		vim.api.nvim_set_current_win(windows[#windows])
 		vim.api.nvim_command("vnew") -- 在右侧创建一个垂直分割的窗口
 		vim.api.nvim_command("edit " .. filename) -- 使用新窗口打开文件
 		require("nvim-tree.view").resize()
@@ -47,6 +33,7 @@ local function open_file_in_specify_window(filename, num)
 	vim.opt.eventignore = eventignore
 end
 
+-- 绑定keymap函数
 local function my_on_attach(bufnr)
 	local api = require("nvim-tree.api")
 
@@ -76,6 +63,7 @@ local function my_on_attach(bufnr)
 	-- custom mapping
 	vim.keymap.set("n", "s", open_file_in_right_side_handle, opts("Open file in right side"))
 	vim.keymap.set("n", "<CR>", open_file_in_left_side_handle, opts("Open file in left side"))
+	vim.keymap.set("n", "<C-n>", api.tree.close, opts("Closes the tree"))
 end
 
 return {
