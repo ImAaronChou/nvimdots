89cbba6d534df2f9c2c1172e59584ed82e4a3953
aaron.zhou <aaron.zhou@narwal.com>
2023-09-07 22:45:51 +0800

    添加range format

--- lua/user_template/keymap/lang.lua
+++ lua/user_template/keymap/lang.lua
@@ -1,1 +1,6 @@
-return {}
+local bind = require("keymap.bind")
+local map_cr = bind.map_cr
+
+return {
+	["n|<leader>lf"] = map_cr("luafile %"):with_noremap():with_silent():with_desc("config update: luafile %"),
+}
