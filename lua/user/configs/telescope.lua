28f01aa4b14700a932e5d75a3f2ed9c57e04c65f
aaron.zhou <aaron.zhou@narwal.com>
2023-09-11 18:18:11 +0800

    添加telescope搜索时，切换目录的功能

--- lua/user/configs/telescope.lua
+++ lua/user/configs/telescope.lua
@@ -1,20 +1,50 @@
+function ts_select_dir_for_grep(prompt_bufnr)
+	local action_state = require("telescope.actions.state")
+	local fb = require("telescope").extensions.file_browser
+	local live_grep = require("telescope").extensions.live_grep_args.live_grep_args
+	local current_line = action_state.get_current_line()
+
+	fb.file_browser({
+		files = false,
+		depth = false,
+		attach_mappings = function(prompt_bufnr)
+			require("telescope.actions").select_default:replace(function()
+				local entry_path = action_state.get_selected_entry().Path
+				local dir = entry_path:is_dir() and entry_path or entry_path:parent()
+				local relative = dir:make_relative(vim.fn.getcwd())
+				local absolute = dir:absolute()
+
+				live_grep({
+					results_title = relative .. "/",
+					cwd = absolute,
+					default_text = current_line,
+				})
+			end)
+
+			return true
+		end,
+	})
+end
+
 return {
 	defaults = {
 		mappings = {
 			n = {
 				["<C-j>"] = require("telescope.actions").move_selection_next,
 				["<C-k>"] = require("telescope.actions").move_selection_previous,
+				["<C-d>"] = ts_select_dir_for_grep,
 			},
 			i = {
 				["<C-j>"] = require("telescope.actions").move_selection_next,
 				["<C-k>"] = require("telescope.actions").move_selection_previous,
+				["<C-d>"] = ts_select_dir_for_grep,
 			},
 		},
 	},
 	extensions = {
 		live_grep_args = {
 			mappings = { -- extend mappings
-				i = {
+				i = { --覆盖原配置
 					["<C-k>"] = require("telescope.actions").move_selection_previous,
 				},
 			},
--- lua/user/plugins/tool.lua
+++ lua/user/plugins/tool.lua
@@ -6,4 +6,13 @@ tool["nvim-tree/nvim-tree.lua"] = {
 	},
 }
 
+tool["nvim-telescope/telescope.nvim"] = {
+	dependencies = {
+		{
+			"nvim-telescope/telescope-file-browser.nvim",
+			dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		},
+	},
+}
+
 return tool
