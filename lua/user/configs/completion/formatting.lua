394f28dda9488a14696fcc9a88454ecb9e2b9ef4
aaron.zhou <aaron.zhou@narwal.com>
2023-09-12 22:50:44 +0800

    添加过程尝试代码

--- lua/user/configs/completion/formatting.lua
+++ lua/user/configs/completion/formatting.lua
@@ -1,3 +1,5 @@
+--当前该文件暂时废弃，直接使用\=快捷键做全局格式化
+--当前的选中格式化无法自动获得最小语法单元，因此如果未框选最小语法单元，不会进行格式化，所以暂时直接用全局格式化替换
 local settings = require("core.settings")
 local format_notify = settings.format_notify
 
@@ -6,6 +8,32 @@ local M = {}
 vim.api.nvim_create_user_command("FormatRange", function()
 	M.format_range({ timeout_ms = 1000 })
 end, {})
+
+--该函数原本想通过treesitter获取最小语法单元，但为尝试成功，当前保留中间代码，通过nvim-treesitter写稍微复杂点应该仍能实现选中块format
+-- local function GetSelectedRows()
+-- 	return { start_row = vim.api.nvim_buf_get_mark(0, "<")[1], vim.api.nvim_buf_get_mark(0, ">")[1] }
+-- end
+
+-- local function GetTSNode(st_row, ed_row)
+-- 	local bufnr = vim.api.nvim_get_current_buf() -- 获取当前缓冲区号
+-- 	-- 解析当前缓冲区的语法树
+-- 	local syntax_tree = vim.treesitter.get_parser(bufnr):parse()
+
+-- 	-- 获取语法树的根节点
+-- 	local root_node = syntax_tree:root()
+
+-- 	-- 获取行号对应的范围
+-- 	local selected_range = {
+-- 		start_row = st_row - 1, -- 行号从 0 开始，需要减去 1
+-- 		end_row = ed_row - 1,
+-- 		start_col = 0,
+-- 		end_col = -1, -- 结束列设为 -1 表示到行尾
+-- 	}
+-- 	local selected_node = root_node:descendant_for_range(selected_range)
+-- 	local node_range = selected_node:range()
+
+-- 	return { start_row = node_range.start.row + 1, end_row = node_range.end.row + 1 }
+-- end
 
 function M.format_range(opts)
 	local bufnr = opts.bufnr or vim.api.nvim_get_current_buf()
@@ -22,10 +50,11 @@ function M.format_range(opts)
 		)
 	end
 
-    --取当前行+上一行，才可format当前行
-    --结束行需要涵盖整个语法单元才会触发format, 当前formatter不支持行号找语法单元,因此暂不支持该功能
+	--取当前行+上一行，才可format当前行
+	--结束行需要涵盖整个语法单元才会触发format, 当前formatter不支持行号找语法单元,因此暂不支持该功能
 	local selected_st_row = math.max(vim.api.nvim_buf_get_mark(0, "<")[1] - 1, 1)
 	local selected_ed_row = vim.api.nvim_buf_get_mark(0, ">")[1]
+
 	local range = {
 		["start"] = {
 			line = selected_st_row,
--- lua/user/keymap/completion.lua
+++ lua/user/keymap/completion.lua
@@ -19,7 +19,10 @@ mappings["lsp"] = function(buf)
 		-- 必须使用map_cu, 为获取visual选中内容，使用的'<' 和 '>'标志必须在visual模式结束才会生效，否则获取的是上一次visual模式的选中内容
 		-- 因此使用map_cu进入命令模式就可以退出visual模式，获得当前选中代码块
 		-- format 对选中的代码块有完整性要求, 如果想要达到选中任意块都可format需要做语法分析获取最小可format单元
-		["nv|<leader>="] = require("keymap.bind").map_cu("FormatRange"):with_buffer(buf):with_desc("lsp: format range"),
+		["nv|<leader>="] = require("keymap.bind")
+			.map_cu("lua vim.lsp.buf.format()")
+			:with_buffer(buf)
+			:with_desc("lsp: format range"),
 		["nv|<leader>sw"] = require("keymap.bind")
 			.map_cu("ClangdSwitchSourceHeader")
 			:with_buffer(buf)
